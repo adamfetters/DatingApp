9bfdbac278d6093efb6829080889ccc753f690e1
adamfetters <adam.fetters@wearesmartdata.com>
2018-08-20 15:35:33 -0400

    Initial Commit

---
+++ Dating_Site.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Dating_Site.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating_Site.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
